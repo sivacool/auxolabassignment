e2669b6f58abca238fa68aa9a7166b4918dfc4e1
sivaravi <sivasivacsc@gmail.com>
2018-12-17 17:59:58 +0530

    indentaion of tests api

--- app.py
+++ app.py
@@ -5,16 +5,16 @@ from multiprocessing import Value
 counter = Value('i', 0)
 
 app = Flask(__name__)
-
+app.config.from_pyfile('config.cfg')
 @app.route('/tests', methods=['POST'])
 def tests():
-		welcome = "Hello "+request.get_json()['name']
-		with counter.get_lock():
-			counter.value += 1
-		app.logger.debug(counter.value)
-		# if counter.value >=1000:
-		# 	app.logger.debug("mailtrigger")
-		return Response(dumps(welcome),mimetype='application/json')
+	welcome = "Hello "+request.get_json()['name']
+	with counter.get_lock():
+		counter.value += 1
+	app.logger.debug(counter.value)
+	# if counter.value >=1000:
+	# 	app.logger.debug("mailtrigger")
+	return Response(dumps(welcome),mimetype='application/json')
 
 if __name__ == '__main__':
-	app.run(debug=True)  
+	app.run()  
