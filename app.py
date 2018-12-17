cac12912fec751bdae61489f1568e48eca5464b9
sivaravi <sivasivacsc@gmail.com>
2018-12-17 22:27:40 +0530

    adding small changes

--- app.py
+++ app.py
@@ -1,20 +1,34 @@
 from flask import Flask, jsonify, request,Response
 from json import dumps,loads
 from multiprocessing import Value
+from flask_mail import Mail,Message
+from datetime import datetime
 
 counter = Value('i', 0)
 
 app = Flask(__name__)
+
+mail = Mail(app)
 app.config.from_pyfile('config.cfg')
+
 @app.route('/tests', methods=['POST'])
 def tests():
+	starttime =datetime.now()
 	welcome = "Hello "+request.get_json()['name']
 	with counter.get_lock():
 		counter.value += 1
-	app.logger.debug(counter.value)
-	# if counter.value >=1000:
-	# 	app.logger.debug("mailtrigger")
+	if counter.value % 1000 == 0:
+		time_elapsed = datetime.now() - starttime
+		app.logger.debug(time_elapsed)
+		app.logger.debug("mailTiggered")
+		msg = Message("Once reach thousands",sender= 'sivasivacsc@gmail.com',recipients=['sivasivacsc@gmail.com'])
+		msg.body = "This testing one"
+		app.logger.debug("!!!!!!!!!!!!!!!!!! Am Gonna send Email !!!!!!!!!!!!!")
+		mail.send(msg)
+		app.logger.debug("!!!!!!!!!!!!!!!!!! Am Gonna send Email !!!!!!!!!!!!!")
+	starttime = datetime.now()
 	return Response(dumps(welcome),mimetype='application/json')
+
 
 if __name__ == '__main__':
 	app.run()  
