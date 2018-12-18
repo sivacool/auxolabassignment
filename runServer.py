26286d650fba2c3ea7f73fd1221220e8a8fd6474
Siva Ravi <sivasivacsc@gmail.com>
2018-12-18 12:16:01 +0530

    adding the filename for nohups

--- runServer.py
+++ runServer.py
@@ -1,3 +1,4 @@
-nohup python3 server.py &
-nohup python3 loadbalancer.py &
+nohup python3 server.py &> server.out&
+nohup python3 loadbalancer.py &> loadbalancer.out&
+
 locust -c 10 -r 5 -t 1m --no-web --host http://localhost:5000 --csv=log
